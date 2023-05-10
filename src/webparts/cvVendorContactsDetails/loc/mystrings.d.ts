dab70438ddac501d96cdae4bea2d130951e00d05
Ankit_CV <ankit@thecodevision.com>
2023-05-10 19:57:40 +0530

    Initial Commit

---
+++ src/webparts/cvVendorContactsDetails/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICvVendorContactsDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CvVendorContactsDetailsWebPartStrings' {
+  const strings: ICvVendorContactsDetailsWebPartStrings;
+  export = strings;
+}
