dd696c4bfa115af9464f427859205379997b42e1
Ankit_CV <ankit@thecodevision.com>
2023-05-10 20:25:20 +0530

    Manage folder structure for components and style sheets.

---
+++ src/components/RequestForm/RequestForm.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { IRequestFormProps } from './IRequestFormProps';
+
+
+
+const RequestForm: React.FunctionComponent<IRequestFormProps> = (props) => {
+    return (
+        <div>
+            Request Form (Form Design)
+        </div>
+    );
+};
+
+export default RequestForm;
---
+++ src/components/VendorContactDetails/VendorContactDetails.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { IVendorContactDetailsProps } from './IVendorContactDetailsProps';
+
+
+const VendorContactDetails: React.FunctionComponent<IVendorContactDetailsProps> = (props) => {
+  return (
+    <div>
+      Vendor Contact Details (Card Design)
+    </div>
+  );
+};
+
+export default VendorContactDetails;
